e03c666d8dc51f54c21173b5156c53bc387a168d
A-Cer23 <85203799+A-Cer23@users.noreply.github.com>
2024-03-04 10:30:15 -0500

    Create fetch to Github API to receive a repo's contents

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+type Bindings = {
+  GITHUB_AUTH_TOKEN: string
+  REPO_URL: string
+}
+
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/', async (c) => {
+
+  const response = await fetch(c.env.REPO_URL, {
+    method: 'GET',
+    headers: {
+      "User-Agent": "hono",
+      "Accepts": "application/vnd.github.raw+json",
+      "Authorization": `Bearer ${c.env.GITHUB_AUTH_TOKEN}`,
+      'X-GitHub-Api-Version': '2022-11-28'
+    },
+  })
+
+  return c.text(JSON.stringify(await response.json(), null, 2))
 })
 
 export default app
